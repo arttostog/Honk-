a0148754d539cccc6af937a52256379145b43f62
arttostog <arttostog@gmail.com>
2023-05-31 00:30:20 +0500

    Project Init

---
+++ Honk!/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using GooseShared;
+
+namespace Honk
+{
+    public class Main : IMod
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        static Keys Key = Keys.F;
+        static bool pressed = false;
+
+        void IMod.Init()
+        {
+            InjectionPoints.PreTickEvent += Tick;
+        }
+
+        public void Tick(GooseEntity goose)
+        {
+            if (GetAsyncKeyState(Main.Key) != 0 && !pressed)
+            {
+                pressed = true;
+                API.Goose.playHonckSound();
+                return;
+            }
+            pressed = false;
+        }
+    }
+}
